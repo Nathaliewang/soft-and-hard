e541247564555cbbe00c5ab40334b6717d4579ad
Hardy.苏锡强 <alwxkxk@qq.com>
2018-05-12 19:10:23 +0800

    添加Part2 exercise2

---
+++ Part2/exercise2/javascripts/baidu-tongji.js
@@ -0,0 +1,14 @@
+/**
+ * 文件：public/javascripts/baidu-tongji.js
+ * 作者：alwxkxk
+ * 修改时间：2018/04/29
+ * 描述：百度统计代码
+ * 整个教程在不断迭代更新中，最新教程获取：https://github.com/alwxkxk/sofe-and-hard
+ */
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement('script')
+  hm.src = 'https://hm.baidu.com/hm.js?5c94ae9b5048679809fcedbd6608121e'
+  var s = document.getElementsByTagName('script')[0]
+  s.parentNode.insertBefore(hm, s)
+})()
